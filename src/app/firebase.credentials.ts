a77707dc6eb5d7caaa9e9080099f89334fb455b1
felpgoes <felipe@devmagic.com.br>
2019-08-10 17:48:42 -0300

    Add firebase conection

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBYVAkp9QqUDlCqcAKXkBXqyYeU6NGc5ZA",
+    authDomain: "listy-backend.firebaseapp.com",
+    databaseURL: "https://listy-backend.firebaseio.com",
+    projectId: "listy-backend",
+    storageBucket: "",
+    messagingSenderId: "487504574683",
+    appId: "1:487504574683:web:8a9f28b2bf75893a"
+  };
