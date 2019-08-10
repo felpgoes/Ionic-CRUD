5ef913c81678d441c35999871d0be4f738f63ea4
felpgoes <felipe@devmagic.com.br>
2019-08-10 17:49:54 -0300

    Update routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'create-list', loadChildren: './create-list/create-list.module#CreateListPageModule' },
+  { path: 'edit-list/:name', loadChildren: './edit-list/edit-list.module#EditListPageModule' },
+  
+
 ];
 
 @NgModule({
