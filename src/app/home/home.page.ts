561459ac65d2ed75be7ff39a6e4eecc20bbd8145
felpgoes <felipe@devmagic.com.br>
2019-08-10 17:49:43 -0300

    Update home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { ListService } from '../services/list/list.service';
+import { Observable } from 'rxjs';
+import { map } from "rxjs/operators";
+import { Item } from '../models/item/item.model';
+import { NavParams, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +12,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  listItem: Observable<Item[]>
+
+  constructor(
+    //public navParams: NavParams,
+    public navCtrl: NavController,
+    private list: ListService
+  ) {
+
+    this.listItem = this.list
+    .getList()
+    .snapshotChanges()
+    .pipe(map(
+      changes =>{
+        return changes.map(c => ({
+          key: c.payload.key, ...c.payload.val()
+        })) 
+      }))
+  }
 
 }
