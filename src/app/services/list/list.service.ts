74b69ca0c5cdd0a5e86cac8672112fff519a223e
felpgoes <felipe.goess@hotmail.com>
2019-08-14 11:11:37 -0300

    Update list services

--- src/app/services/list/list.service.ts
+++ src/app/services/list/list.service.ts
@@ -1,41 +1,61 @@
 import { Injectable } from "@angular/core";
 import { AngularFireDatabase, snapshotChanges } from '@angular/fire/database';
 
-
 import { Item } from 'src/app/models/item/item.model';
+import * as firebase from 'firebase';
+import { getRandomString } from 'selenium-webdriver/safari';
 
-export class ListService{
-    private ListRef= this.db.list<Item>
-    ('list');
+export class ListService {
 
+    id: any
+    database = firebase.database();
+    db = firebase.firestore();
     constructor(
-        private db: AngularFireDatabase
+        private firedb: AngularFireDatabase,
         
-    ){ }
 
-    getList(){
-        return this.ListRef
+    ) {
+
     }
 
-    addItem(item: Item){
-        return this.ListRef.push(item)
+    writeData(name, product, quantity) {
+        this.id = getRandomString()
+
+        this.database.ref('list' + this.id).set({
+            name: name,
+            product: product,
+            quantity: quantity
+        });
+
     }
 
-    async getListByName(name: string){
-       const item = await this.db.database.ref('name').orderByChild('name').equalTo(name).on('value', function(snapshot){
-          
-           
-           snapshot.forEach(function(childSnapshot){
-               var key = childSnapshot.key
-               var childData = childSnapshot.val()
-
-               console.log(childData.product);
-               
+    getAllLists(){
+        let ref = this.db.collection('list').doc()
+        let getLists = ref.get()
+            .then(res => {
+                if (!res.exists) {
+                    console.log('Sem listas');
+                } else {
+                    console.log(res.data());
+                }
             })
-       })
-        
+            .catch(err => {
+                console.log('Deu erro!', err);
+                
+            })
+    }
+
+    getList(listId){
+        const ref = this.database.ref('list' + listId)
+        ref.on('value', (res) => {
+            res.val()
+            console.log(res.val());
             
-        
-       return item
+        })
+    }
+
+
+    async getListById(id) {
+
     }
 }
